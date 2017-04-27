b5d4b5623111991a7a4a377faa36303921064ce8
Assaf <assafyossifoff@sky.com>
2017-04-27 13:22:39 +0100

    added optional v2 generate param and added check gap function

--- src/V2/Receive/Receive.php
+++ src/V2/Receive/Receive.php
@@ -36,23 +36,26 @@ class Receive
     }
 
     /**
-     * Generates a receive adddress. 
+     * Generates a receive adddress.
      *
      * @param string $key The API key.
      * @param string $xpub The public key.
      * @param string $callback The callback URL.
+     * @param int    $gap_limit How many unused addresses are allowed.
      * @return \Blockchain\V2\Receive\ReceiveResponse
      * @throws \Blockchain\Exception\Error
      * @throws \Blockchain\Exception\HttpError
      */
-    public function generate($key, $xpub, $callback)
+    public function generate($key, $xpub, $callback, $gap_limit = null)
     {
         $p = compact('key', 'xpub', 'callback');
+        if(!is_null($gap_limit))
+            $p['gap_limit'] = $gap_limit;
         $q = http_build_query($p);
 
         curl_setopt($this->ch, CURLOPT_POST, false);
         curl_setopt($this->ch, CURLOPT_URL, static::URL.'?'.$q);
-        
+
         if (($resp = curl_exec($this->ch)) === false) {
             throw new HttpError(curl_error($this->ch));
         }
@@ -65,6 +68,41 @@ class Receive
 
         if ($info['http_code'] == 200) {
             return new ReceiveResponse($data['address'], $data['index'], $data['callback']);
+        }
+
+        throw new Error(implode(', ', $data));
+    }
+
+    /**
+    * Get the index gap bewteen the last address
+    * paid to and the last address generated
+    *
+    * @param string $key The API key.
+    * @param string $xpub The public key.
+    * @return int The address gap.
+    * @throws \Blockchain\Exception\Error
+    * @throws \Blockchain\Exception\HttpError
+    */
+    public function checkAddressGap($key, $xpub)
+    {
+        $p = compact('key', 'xpub');
+        $q = http_build_query($p);
+
+        curl_setopt($this->ch, CURLOPT_POST, false);
+        curl_setopt($this->ch, CURLOPT_URL, static::URL.'/checkgap?'.$q);
+
+        if (($resp = curl_exec($this->ch)) === false) {
+            throw new HttpError(curl_error($this->ch));
+        }
+
+        if (($data = json_decode($resp, true)) === NULL) {
+            throw new Error("Unable to decode JSON response from Blockchain: $resp");
+        }
+
+        $info = curl_getinfo($this->ch);
+
+        if ($info['http_code'] == 200) {
+            return $data['gap'];
         }
 
         throw new Error(implode(', ', $data));
